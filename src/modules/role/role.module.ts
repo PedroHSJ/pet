448bfb16b3f9394e5ed780d219c2418ffaad94e3
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-08-11 17:06:33 -0300

    FIX: ROLE MODULE

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleEntity } from './role.entity';
 
 @Module({
-  providers: [RoleService],
-  controllers: [RoleController]
+    imports: [TypeOrmModule.forFeature([RoleEntity])],
+    providers: [RoleService],
+    controllers: [RoleController],
 })
 export class RoleModule {}
