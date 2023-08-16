b47cc88defe3f78a62be4db1ac6111c40bdb5f41
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-08-16 18:50:37 -0300

    FEAT: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     app.setGlobalPrefix('/api/v1');
 
     const config = new DocumentBuilder()
